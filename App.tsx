9045c633a1ce20ffd0af782e51d5736e55930296
Rodriasd <veronrodrigo98@gmail.com>
2024-05-27 16:35:32 -0300

    added the image to background

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./pages/Login";
+import Index from "./pages/Index";
+
 const Stack = createNativeStackNavigator();
 
 
@@ -11,6 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Index" component={Index} options={{headerShown:false}}/>
         <Stack.Screen name="Login" component={Login} options={{headerShown:false}}/>
       </Stack.Navigator>
     </NavigationContainer>
--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import { StyleSheet,View, ImageBackground,Text } from "react-native";
 
 const Index: React.FC = () => {
     return(
-        <>
-        </>
-    )
-}
+        <View style={style.container}>
+            <ImageBackground style={style.backgroundImage} source={require('../assets/wave-haikei.png')}/>
+            <Text>Hola</Text>
+        </View>
+    );
+};
+
+export default Index;
+
+const style = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+    },
+})
