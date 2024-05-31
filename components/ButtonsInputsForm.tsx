a33e7a34246c6031b6fdff812dc27a5b3f0d50a1
Rodriasd <veronrodrigo98@gmail.com>
2024-05-31 12:26:14 -0300

    fixed code

--- components/ButtonsInputsForm.tsx
+++ components/ButtonsInputsForm.tsx
@@ -1,12 +1,54 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text, TouchableOpacity } from "react-native";
 import { StyledTextInput } from "./StyledTextInput";
 import { ScaledSheet } from "react-native-size-matters";
-import { SubmitForm } from "./submits/SubmitForm";
+import { Formik } from "formik";
 
 interface Props {
     username: boolean
 }
+
+const initialState ={
+    name: '',
+    email: '',
+    password: ''
+}
+
+export const ButtonsInputsForm: React.FC<Props> = ({username}) => {
+    return(
+        <Formik initialValues={initialState} onSubmit={values => console.log(values)}>
+           { ({handleSubmit}) => (
+                <View style={styles.container}>
+                        {
+                            username == false ? <View></View>:
+                            <View style={styles.containerInput}>
+                                <Text>Nombre *</Text>
+                                <StyledTextInput/>
+                            </View>
+                        }
+                        <View style={styles.containerInput}>
+                            <Text>Email *</Text>
+                            <StyledTextInput/>
+                        </View>
+                        <View style={styles.containerInput}>
+                            <Text>Contraseña *</Text>
+                            <StyledTextInput/>
+                        </View>
+                        <View style={{flex: 1, height: 20}}>
+                            <TouchableOpacity style={styles.ButtonLogin} onPress={() => handleSubmit}>
+                                <Text style={styles.text}>
+                                    {username == false ? 'Iniciar sesión':'Registrarse'} 
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                )
+            }
+        </Formik>
+    );
+};
+
 
 const styles = ScaledSheet.create({
     container: {
@@ -15,29 +57,21 @@ const styles = ScaledSheet.create({
     },
     containerInput: {
         marginHorizontal: 40,
+    },
+    ButtonLogin:{
+        backgroundColor: '#a04dda',
+        width: '130@s',
+        borderRadius: 20,
+        marginLeft: 20,
+        marginTop: 10,
+        alignSelf: 'center',
+    },
+    text:{
+        textAlign: 'center',
+        fontSize: '19@s',
+        color: 'white',
+        padding: 10,
+        textAlignVertical: 'center'
     }
 })
-
-export const ButtonsInputsForm: React.FC<Props> = ({username}) => {
-    return(
-        <View style={styles.container}>
-            {
-                username == false ? <View></View>:
-                <View style={styles.containerInput}>
-                    <Text>Nombre *</Text>
-                    <StyledTextInput/>
-                </View>
-            }
-            <View style={styles.containerInput}>
-                <Text>Email *</Text>
-                <StyledTextInput/>
-            </View>
-            <View style={styles.containerInput}>
-                <Text>Contraseña *</Text>
-                <StyledTextInput/>
-            </View>
-            <SubmitForm username={username}/>
-        </View>
-    );
-};
 
