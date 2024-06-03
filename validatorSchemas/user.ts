c6aed2a00473a320452cb8ef23d11a71d8511381
Rodriasdev <veronrodrigo98@gmail.com>
2024-06-02 23:23:02 -0300

    was changed validations and added the json of the tasks

--- validatorSchemas/user.ts
+++ validatorSchemas/user.ts
@@ -3,26 +3,32 @@ import * as yup from 'yup';
 export const loginValidationSchema = yup.object().shape({
     email: yup
         .string()
-        .email()
-        .required('email is required'),
+        .email('Debe ser un correo electrónico válido')
+        .required('El correo electrónico es requerido'),
     password: yup
         .string()
-        .min(5, 'Too long!')
-        .max(1000, 'Too long!')
-        .required('Password is required')
+        .min(5, 'La contraseña debe tener al menos 5 caracteres')
+        .matches(/[A-Z]/, 'La contraseña debe contener al menos una letra mayúscula')
+        .matches(/[a-z]/, 'La contraseña debe contener al menos una letra minúscula')
+        .matches(/[@$!%*?&]/, 'La contraseña debe contener al menos un símbolo')
+        .required('La contraseña es requerida')
 });
 
 export const registerValidationSchema = yup.object().shape({
     name: yup
-    .string()
-    .required('name is required'),
+        .string()
+        .min(5, 'El nombre debe tener al menos 5 caracteres')
+        .max(10, 'El nombre no puede tener más de 10 caracteres')
+        .required('El nombre es requerido'),
     email: yup
         .string()
-        .email()
-        .required('email is required'),
+        .email('Debe ser un correo electrónico válido')
+        .required('El correo electrónico es requerido'),
     password: yup
         .string()
-        .min(5, 'Too long!')
-        .max(1000, 'Too long!')
-        .required('Password is required')
+        .min(5, 'La contraseña debe tener al menos 5 caracteres')
+        .matches(/[A-Z]/, 'La contraseña debe contener al menos una letra mayúscula')
+        .matches(/[a-z]/, 'La contraseña debe contener al menos una letra minúscula')
+        .matches(/[@$!%*?&]/, 'La contraseña debe contener al menos un símbolo')
+        .required('La contraseña es requerida')
 });
