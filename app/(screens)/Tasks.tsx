0bce0d639631371687241967f392e0bda654847c
Rodriasd <veronrodrigo98@gmail.com>
2024-06-07 16:30:58 -0300

    changes

--- app/(screens)/Tasks.tsx
+++ app/(screens)/Tasks.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { View } from "react-native";
 import { TaskList } from "@/components/TasksList";
 import tasks from '../../data/tasks.json'
-
+import { ScaledSheet } from "react-native-size-matters";
 
 const Tasks: React.FC = () => {
     return (
         <View>
-            <TaskList tasks={tasks}/>
+            <View>
+                <TaskList tasks={tasks}/>
+            </View>
+            
         </View>
     );
 };
 
 export default Tasks;
+
+const styles = ScaledSheet.create({
+    
+});
--- components/TasksList.tsx
+++ components/TasksList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View, Text, Platform } from "react-native";
 import { findTasks } from "@/types/tasks.dto";
+import { ScaledSheet, scale } from "react-native-size-matters";
 
 interface Props {
     tasks: findTasks[]
@@ -12,10 +13,32 @@ export const TaskList:React.FC<Props> = ({tasks}) => {
             data={tasks}
             keyExtractor={(item) => item.id.toString()}
             renderItem={({item}) => (
-                <View>
-                    <Text>{item.title}</Text>
+                <View style={styles.container}>
+                    <View style={styles.textContainer}>
+                        <Text style={[{fontWeight: 'bold'}]}>Titulo:</Text>
+                        <Text >{item.title}</Text>
+                    </View>
+                    
+                    <Text>{item.author}</Text>
+                    <Text>{item.description}</Text>
+                    <Text>{item.date}</Text>
                 </View>
             )}
         />
     );
 };
+
+const styles = ScaledSheet.create({
+    container: {
+        borderWidth: 1,
+        borderRadius: scale(10),
+        marginHorizontal: scale(10),
+        marginVertical: scale(10),
+        padding: scale(10),
+        alignSelf: 'center',
+        width: Platform.OS == "web" ? "50%" : "90%"
+    },
+    textContainer: {
+        flexDirection: "row"
+    }
+})
